6d627cac68302fc9a82151e83c4eeaa775a5e972
Tristan van Triest <tristantriest@gmail.com>
2020-10-13 15:27:19 +0200

    Added styling to everything

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,19 +1,21 @@
 import React from 'react';
-
+import style from './recipe.module.css';
 
 const Recipe = ({title, calories, image, ingredients}) => {
+
+
+
     return(
-        <div>
-            <h1>{title}</h1>
+        <div className={style.recipe}>
+            <h1 >{title}</h1>
             <p>Amount of calories:{calories}</p>
-            <p>Required ingredients: 
-                <ol>
-                    {ingredients.map(ingredient => (
-                        <li>{ingredient.text}</li>
-                    ))}
-                </ol>
-            </p>
-            <img src={image} alt="" />
+            <p>Required ingredients: </p>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <img className={style.image} src={image} alt="" />
         </div>
     );
 }
